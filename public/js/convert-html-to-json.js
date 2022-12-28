477f70809fb82eb31ce4a5035bb06584afe014f1
chinh.vucong <chinh.vucong@vti.com.vn>
2022-12-28 11:52:16 +0900

    20221228 Add Tool Convert html to json

---
+++ public/js/convert-html-to-json.js
@@ -0,0 +1,225 @@
+console.log("my scripts");
+const SPLIT_CARD = "#####";
+const SPLIT_QUESTION = "\n\n\n";
+$("#split_card").text(SPLIT_CARD);
+$("#split_question").html("\\n\\n\\n");
+
+class Question {
+    constructor(questionNumber, questionBody, questionChoices, correctAnswer, votedAnswer, comments = []) {
+        this.questionNumber = questionNumber;
+        this.questionBody = questionBody;
+        this.questionChoices = questionChoices;
+        this.correctAnswer = correctAnswer;
+        this.votedAnswer = votedAnswer;
+        this.comments = comments;
+    }
+
+    getCorrectAnswer() {
+        if(this.votedAnswer == "") {
+            return this.correctAnswer;
+        } else {
+            return this.votedAnswer.replace(/[\s]*\([0-9]*\%\)/gm,"");
+        }
+    }
+
+    exportCSV() {
+        var csvTemplate = `${this.questionNumber}\n${this.questionBody}${this.questionChoices.join("\n")} ${SPLIT_CARD} ${this.getCorrectAnswer()}`
+        return csvTemplate;
+    }
+
+    exportJson() {
+        let answerText = [];
+        let correctAnswer = this.getCorrectAnswer();
+        let SYMBOL_ANSWER = ["A", "B", "C", "D", "E", "F", "G", "H"];
+        this.questionChoices.forEach(function(choice, index) {
+            let item = `
+            {
+                "choice": "<p>${choice}</p>",
+                "correct": ${ correctAnswer.indexOf(SYMBOL_ANSWER[index]) > -1 ? "true" : "false"},
+                "feedback": ""
+            }`;
+
+            answerText = [...answerText, item];
+        });
+
+        answerText = `[
+            ${answerText.join(",")}
+        ]`
+        var csvTemplate = `
+        {
+            "question_id": "${this.questionNumber}",
+            "topic_id": 1,
+            "course_id": 1,
+            "case_study_id": null,
+            "lab_id": 0,
+            "question_text": "<p>${this.questionBody}</p>",
+            "mark": 1,
+            "is_partially_correct": ${correctAnswer.length >= 2 ? "true" : "false"},
+            "question_type": "1",
+            "difficulty_level": "0",
+            "general_feedback": "<p>Correct Answer: ${correctAnswer}</p>",
+            "is_active": true,
+            "answer_list": [
+              {
+                "question_answer_id": 1,
+                "question_id": "${this.questionNumber}",
+                "answers": ${answerText}
+              }
+            ],
+            "topic_name": "",
+            "discusstion": ${JSON.stringify(this.comments)}
+        }
+        `
+        return csvTemplate;
+    }
+}
+
+function commonReplace(text) {
+    text = text.replaceAll('"', '\\"');
+    text = text.replaceAll('  ', '');
+    text = text.replaceAll('\n\n\n', '');
+    text = text.replaceAll('\n', '<br>');
+    text = text.replaceAll('assets/media', 'https://examtopics.com/assets/media');
+    text = text.replaceAll('<br>Most Voted<br>', '');
+
+    text = text.replaceAll('A.', 'A. ');
+    text = text.replaceAll('B.', 'B. ');
+    text = text.replaceAll('C.', 'C. ');
+    text = text.replaceAll('D.', 'D. ');
+    text = text.replaceAll('E.', 'E. ');
+    text = text.replaceAll('F.', 'F. ');
+
+    return text;
+}
+
+var questionBlock;
+var listQuestion = [];
+var question;
+function convert(html) {
+    questionBlock = $(html);
+    $("#tempHtml").html(questionBlock);
+
+    $("#tempHtml .question-title-topic").remove();
+    var questionNumber = $("#tempHtml .card-header").text();
+    questionNumber = questionNumber.match(/#([0-9]{1,4})/g)[0];
+
+    // Question
+    var questionBody = $("#tempHtml .question-body > p.card-text:first-child").html();
+    questionBody = commonReplace(questionBody);
+
+    // Answers
+    var questionChoicesHtml = $("#tempHtml .question-body .question-choices-container ul li");
+    var questionChoices = [];
+    questionChoicesHtml.each((key, questionChoice) => {
+        questionChoiceText = questionChoice.innerText;
+        questionChoiceText = commonReplace(questionChoiceText);
+        questionChoices.push(questionChoiceText);
+    });
+    
+    // Correct Answer
+    var correctAnswer = $("#tempHtml .correct-answer").text();
+    var votedAnswer = $("#tempHtml .vote-distribution-bar .vote-bar.bg-primary").text()
+
+    // Discustion
+    var comments = [];
+    var discussionsHtml = $("#tempHtml .discussion-container .media.comment-container");
+    if (discussionsHtml != "") {
+        discussionsHtml.each((key, commentHtml) => {
+            $("#tempHtmlComment").html(commentHtml);
+    
+            let id = $("#tempHtmlComment .media.comment-container").data("comment-id");
+            let date = $("#tempHtmlComment .comment-date").attr("title");
+            let username = $("#tempHtmlComment .comment-username").text();
+            let content = $("#tempHtmlComment .comment-content").text();
+            let upvote_count = $("#tempHtmlComment .upvote-count").text();
+
+            username = username.replaceAll('  ', '').replaceAll('\n', '');
+            content = commonReplace(content);
+            
+            let comment = {
+                "id": id,
+                "date": date,
+                "username": username,
+                "content": content,
+                "upvote_count": upvote_count
+            };
+    
+            comments.push(comment);
+        });
+    }
+    
+    question = new Question(questionNumber, questionBody, questionChoices, correctAnswer, votedAnswer, comments);
+
+    return question;
+}
+
+function convertFreeCam(html) {
+    questionBlock = $(html);
+    $("#tempHtml").html(questionBlock);
+
+    $("#tempHtml .question-title-topic").remove();
+    var questionNumber = $("#tempHtml h4 span.text-danger").text();
+
+    var questionBody = $("#tempHtml .qa-question").html();
+    questionBody = questionBody.replaceAll('"', '\\"');
+    questionBody = questionBody.replaceAll('  ', '');
+    questionBody = questionBody.replaceAll('\n\n\n', '');
+    questionBody = questionBody.replaceAll('\n', '<br>');
+    questionBody = questionBody.replaceAll('assets/media', 'https://examtopics.com/assets/media');
+
+    var questionChoicesHtml = $("#tempHtml .qa-options > p");
+    var questionChoices = [];
+    questionChoicesHtml.each((key, questionChoice) => {
+        questionChoiceText = questionChoice.innerText;
+        questionChoiceText = questionChoiceText.replaceAll('  ', '');
+        questionChoiceText = questionChoiceText.replaceAll('\n', '');
+        questionChoiceText = questionChoiceText.replaceAll('A.', '');
+        questionChoiceText = questionChoiceText.replaceAll('B.', '');
+        questionChoiceText = questionChoiceText.replaceAll('C.', '');
+        questionChoiceText = questionChoiceText.replaceAll('D.', '');
+        questionChoiceText = questionChoiceText.replaceAll('E.', '');
+        questionChoiceText = questionChoiceText.replaceAll('F.', '');
+        questionChoiceText = questionChoiceText.replaceAll('"', '\\"');
+        questionChoiceText = questionChoiceText.replaceAll('assets/media', 'https://examtopics.com/assets/media');
+        questionChoices.push(questionChoiceText);
+    });
+    
+    var correctAnswer = $("#tempHtml .qa-answerexp span").text();
+
+    var votedAnswer = $("#tempHtml .qa-answerexp span").text()
+
+    question = new Question(questionNumber, questionBody, questionChoices, correctAnswer, votedAnswer)
+
+    return question;
+}
+
+function splitQuestionRaw(htmlRaw) {
+    $("#tempHtml").html(htmlRaw);
+
+    return $("#tempHtml .exam-question-card");
+}
+
+$("#btn-submit-json").on("click", function () {
+    var htmlRaw = $("#html-source").val();
+    var listQuestionRawHtml = splitQuestionRaw(htmlRaw);
+
+    var csvText = "";
+    listQuestion = [];
+    jsonText = [];
+    listQuestionRawHtml.each((key, questionRaw) => {
+        var question = convert(questionRaw);
+        listQuestion.push(question);
+        
+        //convert jsonText
+        jsonText = [...jsonText, question.exportJson()];
+    });
+
+    csvText = `
+    {
+        "msg": "Quiz Questions",
+        "data": [${jsonText.join(",")}]
+    }
+    `;
+
+    $("#csv-result").text(csvText);
+});
